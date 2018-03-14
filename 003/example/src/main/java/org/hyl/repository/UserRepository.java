489a20328089374b9b7192b23d8c6cd2abb957ad
huang6349 <252683858@qq.com>
2018-03-14 16:53:44 +0800

    003

--- 003/example/src/main/java/org/hyl/repository/UserRepository.java
+++ 003/example/src/main/java/org/hyl/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package org.hyl.repository;
 
 import org.hyl.domain.MyUser;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<MyUser, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<MyUser> findOneWithAuthoritiesByUsername(String username);
 }
