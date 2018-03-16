2eded53176958e8171f9b41abc0a88f0db6e867d
huang6349 <252683858@qq.com>
2018-03-16 17:03:58 +0800

    003

---
+++ 003/commons/src/main/java/org/hyl/commons/domain/ResultEntity.java
@@ -0,0 +1,115 @@
+package org.hyl.commons.domain;
+
+import java.io.Serializable;
+
+public class ResultEntity implements Serializable {
+
+    private static final long serialVersionUID = -118029445301983596L;
+
+    private Integer state;
+
+    private String message;
+
+    private Object data;
+
+    private Boolean success;
+
+    private String e;
+
+    private Object params;
+
+    public ResultEntity() {
+    }
+
+    public ResultEntity(Integer state, String message, Object data, Boolean success) {
+        this.state = state;
+        this.message = message;
+        this.data = data;
+        this.success = success;
+    }
+
+    public ResultEntity(Integer state, String message, Object data, Boolean success, Object params) {
+        this.state = state;
+        this.message = message;
+        this.data = data;
+        this.success = success;
+        this.params = params;
+    }
+
+    public ResultEntity(Integer state, String message, Object data, Boolean success, String e) {
+        this.state = state;
+        this.message = message;
+        this.data = data;
+        this.success = success;
+        this.e = e;
+    }
+
+    public ResultEntity(Integer state, String message, Object data, Boolean success, String e, Object params) {
+        this.state = state;
+        this.message = message;
+        this.data = data;
+        this.success = success;
+        this.e = e;
+        this.params = params;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
+
+    public Object getParams() {
+        return params;
+    }
+
+    public void setParams(Object params) {
+        this.params = params;
+    }
+
+    @Override
+    public String toString() {
+        return "ResultEntity{" +
+                "state=" + state +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                ", success=" + success +
+                ", e='" + e + '\'' +
+                ", params=" + params +
+                '}';
+    }
+}
---
+++ 003/commons/src/main/java/org/hyl/commons/utils/ResultUtil.java
@@ -0,0 +1,184 @@
+package org.hyl.commons.utils;
+
+import org.hyl.commons.domain.ResultEntity;
+import org.hyl.commons.enums.Result;
+import org.hyl.commons.service.dto.ResultDTO;
+
+public final class ResultUtil {
+
+    private ResultUtil() {
+    }
+
+    public static ResultEntity success() {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, null, null, null, null));
+    }
+
+    public static ResultEntity success(Result.Type type) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, null, null, null, null));
+    }
+
+    public static ResultEntity success(Integer state) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, state, null, null, null));
+    }
+
+    public static ResultEntity success(Result.Type type, Integer state) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, state, null, null, null));
+    }
+
+    public static ResultEntity success(Integer state, Object data) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, state, null, data, null));
+    }
+
+    public static ResultEntity success(Result.Type type, Integer state, Object data) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, state, null, data, null));
+    }
+
+    public static ResultEntity success(Integer state, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, state, null, data, params));
+    }
+
+    public static ResultEntity success(Result.Type type, Integer state, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, state, null, data, params));
+    }
+
+    public static ResultEntity success(String message) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, null, message, null, null));
+    }
+
+    public static ResultEntity success(Result.Type type, String message) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, null, message, null, null));
+    }
+
+    public static ResultEntity success(String message, Object data) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, null, message, data, null));
+    }
+
+    public static ResultEntity success(Result.Type type, String message, Object data) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, null, message, data, null));
+    }
+
+    public static ResultEntity success(String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, null, message, data, params));
+    }
+
+    public static ResultEntity success(Result.Type type, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, null, message, data, params));
+    }
+
+    public static ResultEntity success(Integer state, String message) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, state, message, null, null));
+    }
+
+    public static ResultEntity success(Result.Type type, Integer state, String message) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, state, message, null, null));
+    }
+
+    public static ResultEntity success(Integer state, String message, Object data) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, state, message, data, null));
+    }
+
+    public static ResultEntity success(Result.Type type, Integer state, String message, Object data) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, state, message, data, null));
+    }
+
+    public static ResultEntity success(Integer state, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.SUCCESS, Result.Type.DEFAULT, state, message, data, params));
+    }
+
+    public static ResultEntity success(Result.Type type, Integer state, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.SUCCESS, type, state, message, data, params));
+    }
+
+    public static ResultEntity error(String e) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, null, null, null, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e) {
+        return response(new ResultDTO(Result.Network.ERROR, type, null, null, null, e, null));
+    }
+
+    public static ResultEntity error(String e, Integer state) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, state, null, null, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, Integer state) {
+        return response(new ResultDTO(Result.Network.ERROR, type, state, null, null, e, null));
+    }
+
+    public static ResultEntity error(String e, Integer state, Object data) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, state, null, data, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, Integer state, Object data) {
+        return response(new ResultDTO(Result.Network.ERROR, type, state, null, data, e, null));
+    }
+
+    public static ResultEntity error(String e, Integer state, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, state, null, data, e, params));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, Integer state, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.ERROR, type, state, null, data, e, params));
+    }
+
+    public static ResultEntity error(String e, String message) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, null, message, null, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, String message) {
+        return response(new ResultDTO(Result.Network.ERROR, type, null, message, null, e, null));
+    }
+
+    public static ResultEntity error(String e, String message, Object data) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, null, message, data, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, String message, Object data) {
+        return response(new ResultDTO(Result.Network.ERROR, type, null, message, data, e, null));
+    }
+
+    public static ResultEntity error(String e, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, null, message, data, e, params));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.ERROR, type, null, message, data, e, params));
+    }
+
+    public static ResultEntity error(String e, Integer state, String message) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, state, message, null, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, Integer state, String message) {
+        return response(new ResultDTO(Result.Network.ERROR, type, state, message, null, e, null));
+    }
+
+    public static ResultEntity error(String e, Integer state, String message, Object data) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, state, message, data, e, null));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, Integer state, String message, Object data) {
+        return response(new ResultDTO(Result.Network.ERROR, type, state, message, data, e, null));
+    }
+
+    public static ResultEntity error(String e, Integer state, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.ERROR, Result.Type.DEFAULT, state, message, data, e, params));
+    }
+
+    public static ResultEntity error(Result.Type type, String e, Integer state, String message, Object data, Object params) {
+        return response(new ResultDTO(Result.Network.ERROR, type, state, message, data, e, params));
+    }
+
+    private static ResultEntity response(ResultDTO resultDTO) {
+        Result.Network network = resultDTO.getNetwork();
+        Result.Type type = resultDTO.getType();
+        ResultEntity result = new ResultEntity();
+        result.setState(type.getState(network, resultDTO.getState()));
+        result.setMessage(type.getMessage(network, resultDTO.getMessage()));
+        result.setData(resultDTO.getData());
+        result.setSuccess(network.isSuccess());
+        result.setE(resultDTO.getE());
+        result.setParams(resultDTO.getParams());
+        return result;
+    }
+}
