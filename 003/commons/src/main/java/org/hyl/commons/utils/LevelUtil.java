ea12efbbc5c3d02b1e304f0e538f2286789a0473
huang6349 <252683858@qq.com>
2018-03-19 16:42:14 +0800

    003

---
+++ 003/commons/src/main/java/org/hyl/commons/utils/LevelUtil.java
@@ -0,0 +1,21 @@
+package org.hyl.commons.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class LevelUtil {
+
+    public final static String SEPARATOR = ".";
+
+    public final static String ROOT = "0";
+
+    public static String calculateLevel() {
+        return ROOT;
+    }
+
+    public static String calculateLevel(String parentLevel, long parentId) {
+        if (StringUtils.isBlank(parentLevel)) {
+            return calculateLevel();
+        }
+        return StringUtils.join(parentLevel, SEPARATOR, parentId);
+    }
+}
---
+++ 003/commons/src/test/java/org/hyl/commons/utils/LevelUtilTest.java
@@ -0,0 +1,29 @@
+package org.hyl.commons.utils;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LevelUtilTest {
+
+    @Test
+    public void calculateLevel0() throws Exception {
+        String a = LevelUtil.calculateLevel();
+        String b = "0";
+        assertThat(a).isEqualTo(b);
+    }
+
+    @Test
+    public void calculateLevel1() throws Exception {
+        String a = LevelUtil.calculateLevel(null, 1);
+        String b = "0";
+        assertThat(a).isEqualTo(b);
+    }
+
+    @Test
+    public void calculateLevel2() throws Exception {
+        String a = LevelUtil.calculateLevel("0", 1);
+        String b = "0.1";
+        assertThat(a).isEqualTo(b);
+    }
+}
