f42eb7351fa6736b6515ee964ae8e3c177a6bc58
huang6349 <252683858@qq.com>
2018-03-27 10:46:36 +0800

    003

--- 003/example/src/main/java/org/hyl/errors/ExceptionHandling.java
+++ 003/example/src/main/java/org/hyl/errors/ExceptionHandling.java
@@ -1,45 +1,8 @@
 package org.hyl.errors;
 
-import org.hyl.commons.result.domain.Message;
-import org.hyl.commons.errors.InternalServerErrorException;
-import org.hyl.commons.result.ResultUtil;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.hyl.base.errors.ThrowableAdviceTrait;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ExceptionHandling {
-
-    @ExceptionHandler(value = InternalServerErrorException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Message handleInternalServerErrorException(InternalServerErrorException e) {
-        return ResultUtil.error(e.getType(), e.getClass().getName(), e.getState(), e.getMessage(), e.getData(), e.getParams());
-    }
-
-    @ExceptionHandler(BindException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Message handleBindException(BindException e) {
-        return ResultUtil.error(e.getClass().getName(), HttpStatus.BAD_REQUEST.value(), e.getBindingResult().getFieldError().getDefaultMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Message handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResultUtil.error(e.getClass().getName(), HttpStatus.BAD_REQUEST.value(), e.getBindingResult().getFieldError().getDefaultMessage());
-    }
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Message handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        return ResultUtil.error(e.getClass().getName(), HttpStatus.BAD_REQUEST.value());
-    }
+public class ExceptionHandling implements ThrowableAdviceTrait {
 }
